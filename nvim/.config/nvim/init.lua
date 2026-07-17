42c09c52f2d0aec66bbeaa4de66f428fb9c73d25
tkanzaki-cmg <t_kanzaki@cm-group.co.jp>
2026-07-17 17:53:38 +0900

    Add nvim and wezterm config

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,14 @@
+-- 背景色を透明に
+vim.api.nvim_set_hl(0, "Normal", {
+  bg = "NONE",
+  ctermbg = "NONE",
+  update = true,
+})
+
+-- 行番号表示
+vim.opt.number = true
+
+-- 相対行表示
+vim.opt.relativenumber = true
+
+require("config.lazy")
---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,33 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- コンフィグ自動反映
+config.automatically_reload_config = true
+
+-- フォントサイズ
+config.font_size = 12.0
+
+-- 透明度
+config.window_background_opacity = 0.7
+
+-- ぼかし
+config.macos_window_background_blur = 4
+
+-- タイトルバーの削除
+config.window_decorations = "RESIZE"
+
+-- タブが１つしかないときに非表示
+config.hide_tab_bar_if_only_one_tab = true
+
+-- タブバーをコンパクトに
+config.use_fancy_tab_bar = false
+
+-- カラースキーマを取得
+-- config.color_scheme = 'AdventureTime'
+-- config.color_scheme = 'Cyberdyne'
+config.color_scheme = 'Eldritch'
+
+return config
