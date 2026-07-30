fe3ca14820a862399514b6a6f5ffbbdafc8a148b
tkanzaki-cmg <t_kanzaki@cm-group.co.jp>
2026-07-30 15:35:56 +0900

    wezterm: use catppuccin mocha

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -26,8 +26,8 @@ config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = false
 
 -- カラースキーマを取得
-config.color_scheme = 'AdventureTime'
--- config.color_scheme = 'cyberpunk'
+-- config.color_scheme = 'AdventureTime'
+config.color_scheme = 'Catppuccin Mocha'
 -- config.color_scheme = 'Eldritch'
 
 -- マルチディスプレイで別モニターをフォーカス時にノッチ回避機能の裏まで表示
