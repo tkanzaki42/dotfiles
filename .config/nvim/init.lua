0539000e9ae4e527b0950266c6a2e34a1bd12f17
tkanzaki-cmg <t_kanzaki@cm-group.co.jp>
2026-07-31 17:40:19 +0900

    add neovim line move keymaps

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -14,5 +14,13 @@ vim.opt.relativenumber = true
 -- ヤンクをシステムクリップボードに共有
 vim.opt.clipboard = "unnamedplus"
 
+-- Option + 矢印で行を上下に移動
+vim.keymap.set("n", "<A-Up>", ":m .-2<CR>==", { desc = "Move line up", silent = true })
+vim.keymap.set("n", "<A-Down>", ":m .+1<CR>==", { desc = "Move line down", silent = true })
+vim.keymap.set("v", "<A-Up>", ":m '<-2<CR>gv=gv", { desc = "Move selection up", silent = true })
+vim.keymap.set("v", "<A-Down>", ":m '>+1<CR>gv=gv", { desc = "Move selection down", silent = true })
+vim.keymap.set("i", "<A-Up>", "<Esc>:m .-2<CR>==gi", { desc = "Move line up", silent = true })
+vim.keymap.set("i", "<A-Down>", "<Esc>:m .+1<CR>==gi", { desc = "Move line down", silent = true })
+
 -- lazy.nvim
 require("config.lazy")
